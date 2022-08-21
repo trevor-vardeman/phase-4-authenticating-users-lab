7b078b422e7d02c1faba2fe9678856d38504808a
Trevor Vardeman <trevortx@gmail.com>
2022-08-21 13:07:46 -0700

    commit tree bab9808ac793635cb79c011a62e08a1a6f4a24ee

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
 end
